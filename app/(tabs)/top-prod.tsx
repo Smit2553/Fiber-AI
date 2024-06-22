49cb22be4ff1e88d49ca4e7d2a4f50d32e1e89df
laerfugi <rquiambao@mail.sfsu.edu>
2024-06-22 15:56:56 -0700

    created top products page WIP

--- app/(tabs)/top-prod.tsx
+++ app/(tabs)/top-prod.tsx
@@ -1,18 +1,81 @@
-import {View, StyleSheet} from 'react-native';
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
 import {ThemedText} from '@/components/ThemedText';
+import {ThemedView} from '@/components/ThemedView';
+import {useState} from 'react';
 
 export default function topProducts() {
-return (
-    <View style={styles.test}>
-        <ThemedText style={{color: "#000"}}>top products page</ThemedText>
-    </View>
-);
+    const [items, setItems] = useState<string[]>(['blank']);
+
+    const [showPreview, setShowPreview] = useState(0);
+
+    const handleButtonClick = (index: number) => {
+        // 0 = none, 1 = food, 2 = drink, 3 = snack
+        console.log(showPreview);
+        if (index === showPreview) {
+            setShowPreview(0);
+        } else {
+            grabItems(index);
+            setShowPreview(index);
+        }
+    }
+
+    const grabItems = (index: number) => {
+        // 1 = food, 2 = drink, 3 = snack
+        // grab top items for each
+        if (index === 1) { 
+            setItems(['food1', 'food2', 'food3']); // placeholders
+        } else if (index === 2) {
+            setItems(['drink1', 'drink2', 'drink3']);
+        } else if (index === 3) {
+            setItems(['snack7', 'snack8', 'snack9']);
+        }
+    }
+
+    return (
+        <ThemedView>
+            <div style={styles.mainContainer}>
+            <Text style={{color: '#000000'}} type='title'>Top Products</Text>
+            </div>
+            <div className='contentContainer'>
+                <button onClick={() => {handleButtonClick(1)}}>
+                    <ThemedText>Food</ThemedText>
+                </button>
+                <button onClick={() => {handleButtonClick(2)}}>
+                    <ThemedText>Drink</ThemedText>
+                </button>
+                <button onClick={() => {handleButtonClick(3)}}>
+                    <ThemedText>Snack</ThemedText>
+                </button>
+            </div>
+
+            {/* conditionally render items */}
+            {showPreview > 0 && (
+                    <ThemedView>
+                        {items.map((item, index) => (
+                            <ThemedText key={index}>{item}</ThemedText>
+                        ))}
+                    </ThemedView>
+                )}
+        </ThemedView>
+    );
 }
 
 const styles = StyleSheet.create({
-    test: {
-        flexDirection: 'row',
+    mainContainer: {
+        // flexDirection: 'row',
         alignItems: 'center',
-        gap: 8,
+        // gap: 8,
     },
+    mainText: {
+        color: 'black',
+    },
+    contentContainer: {
+        // flexDirection: 'row',
+        // alignItems: 'center',
+        // gap: 8,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
 });
---
+++ components/DataEntry.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { ThemedView } from './ThemedView';
+
+export function DataEntry() {
+    return (
+        <ThemedView style={styles.test}>
+        </ThemedView>
+    );
+}
+
+const styles = StyleSheet.create({
+    test: {
+        
+    },
+});
