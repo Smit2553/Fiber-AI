75b11c0b9fdb2a57f9fb72eb3f9f34eadcd1410d
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-06-22 14:15:13 -0700

    Added more navigation
    Co-Authored-By: Andy Le <136204727+4ndy1e@users.noreply.github.com>
    Co-Authored-By: Smit <107775677+Smit2553@users.noreply.github.com>
    Co-Authored-By: Ralph Quiambao <91401874+laerfugi@users.noreply.github.com>

---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Button, Text, View } from "react-native";
+
+export default function search() {
+  return <View></View>;
+}
---
+++ app/(tabs)/star.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { View } from "react-native";
+
+export default function star() {
+  return <View></View>;
+}
