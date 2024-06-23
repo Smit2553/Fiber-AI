8dd03412f18819428754ce7eae17028c787da726
Smit <smitdev2553@gmail.com>
2024-06-22 18:43:09 -0700

    chore: Update .gitignore and Flask API configuration
    The changes include updating the .gitignore file to include the .env file and modifying the Flask API configuration to load environment variables from the .env file.

--- Backend/api.py
+++ Backend/api.py
@@ -1,8 +1,11 @@
 import openfoodfacts
 from flask import Flask, request, jsonify
 from flask_cors import CORS 
+from dotenv import load_dotenv
+import os
 
 
+load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
@@ -21,4 +24,4 @@ def search(code):
 
 if __name__ == '__main__':
     # Update host to your local IP address
-    app.run(debug=True, host='10.56.224.52', port=5050)
+    app.run(debug=True, host=os.environ.get("EXPO_PUBLIC_DEVICEIP"), port=5050)
